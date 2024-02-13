9bc2d8cf983fdb4d97238229c5698f1d0adc3fda
amanueldemelash12 <amanueldemelash12@gmail.com>
2024-02-13 09:48:13 +0300

    categories and search bar

--- lib/app/modules/auth/views/auth_view.dart
+++ lib/app/modules/auth/views/auth_view.dart
@@ -9,118 +9,133 @@ class AuthView extends GetView<AuthController> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          leading: IconButton(
-              onPressed: () => Get.back(),
-              icon: const Icon(
-                Icons.arrow_back_ios,
-              )),
-          elevation: 0,
-        ),
-        body: ListView(padding: const EdgeInsets.all(15), children: [
-          const SizedBox(height: 60),
-          const Text(
-            "Bazaar",
-            textAlign: TextAlign.center,
-            style: TextStyle(
-                fontWeight: FontWeight.bold,
-                color: ColorConstant.primeryColor,
-                fontSize: 26,
-                letterSpacing: 10),
-          ),
-          const SizedBox(
-            height: 40,
-          ),
-          Container(
-            width: Get.width,
-            padding: const EdgeInsets.all(15),
-            child: Form(
-                child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                const Text("Sign In"),
-                const SizedBox(
-                  height: 20,
-                ),
-                TextFormField(
-                  decoration: const InputDecoration(
-                    filled: true,
-                    hintText: "email",
-                    prefixIcon: Icon(
-                      Icons.person,
-                      color: ColorConstant.primeryColor,
-                    ),
-                    border: UnderlineInputBorder(
-                        borderSide:
-                            BorderSide(color: ColorConstant.backgroundColor)),
-                    disabledBorder: UnderlineInputBorder(
-                        borderSide:
-                            BorderSide(color: ColorConstant.primeryColor)),
-                    enabledBorder: UnderlineInputBorder(
-                        borderSide:
-                            BorderSide(color: ColorConstant.primeryColor)),
-                    errorBorder: UnderlineInputBorder(
-                        borderSide:
-                            BorderSide(color: ColorConstant.primeryColor)),
-                  ),
-                ),
-                const SizedBox(
-                  height: 15,
-                ),
-                TextFormField(
-                  decoration: const InputDecoration(
-                    filled: true,
-                    hintText: "password",
-                    prefixIcon: Icon(
-                      Icons.lock,
-                      color: ColorConstant.primeryColor,
-                    ),
-                    border: UnderlineInputBorder(
-                        borderSide:
-                            BorderSide(color: ColorConstant.backgroundColor)),
-                    disabledBorder: UnderlineInputBorder(
-                        borderSide:
-                            BorderSide(color: ColorConstant.primeryColor)),
-                    enabledBorder: UnderlineInputBorder(
-                        borderSide:
-                            BorderSide(color: ColorConstant.primeryColor)),
-                    errorBorder: UnderlineInputBorder(
-                        borderSide:
-                            BorderSide(color: ColorConstant.primeryColor)),
-                  ),
-                ),
-                const SizedBox(
-                  height: 50,
-                ),
-                SizedBox(
-                  width: Get.width,
-                  child: ElevatedButton(
-                      onPressed: () {
-                        Get.toNamed(Routes.HOME);
-                      },
-                      child: const Text(
-                        "Sign In",
-                        style: TextStyle(color: ColorConstant.backgroundColor),
-                      )),
-                )
-              ],
-            )),
-          ),
-          SizedBox(
-              child: Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              const Text("Don`t have an account "),
-              InkWell(
-                  onTap: () {},
-                  child: const Text(
-                    "SIGNUP",
-                    style: TextStyle(
-                        fontWeight: FontWeight.bold,
-                        color: ColorConstant.primeryColor),
+        body:Stack(
+          children: [
+            Container(
+              width: Get.width,
+              height: Get.height,
+              decoration:const BoxDecoration(
+                image: DecorationImage(image: AssetImage("assets/images/back.jpg"),fit: BoxFit.fill)
+              ),
+            ),
+            Container(
+              width: Get.width,
+              height: Get.height,
+              color: ColorConstant.primeryColor.withOpacity(0.4),
+            ),
+            ListView(padding: const EdgeInsets.all(15), children: [
+              const SizedBox(height: 60),
+              const Text(
+                "Bazaar",
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    color: ColorConstant.backgroundColor,
+                    fontSize: 26,
+                    letterSpacing: 10),
+              ),
+              const SizedBox(
+                height: 40,
+              ),
+              Container(
+                width: Get.width,
+                padding: const EdgeInsets.all(15),
+                child: Form(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        const Text("Sign In"),
+                        const SizedBox(
+                          height: 20,
+                        ),
+                        TextFormField(
+                          decoration: const InputDecoration(
+                            filled: true,
+                            hintText: "email",
+                            prefixIcon: Icon(
+                              Icons.person,
+                              color: ColorConstant.primeryColor,
+                            ),
+                            border: UnderlineInputBorder(
+                                borderSide:
+                                BorderSide(color: ColorConstant.backgroundColor)),
+                            disabledBorder: UnderlineInputBorder(
+                                borderSide:
+                                BorderSide(color: ColorConstant.primeryColor)),
+                            enabledBorder: UnderlineInputBorder(
+                                borderSide:
+                                BorderSide(color: ColorConstant.primeryColor)),
+                            errorBorder: UnderlineInputBorder(
+                                borderSide:
+                                BorderSide(color: ColorConstant.primeryColor)),
+                          ),
+                        ),
+                        const SizedBox(
+                          height: 15,
+                        ),
+                        TextFormField(
+                          decoration: const InputDecoration(
+                            filled: true,
+                            hintText: "password",
+                            prefixIcon: Icon(
+                              Icons.lock,
+                              color: ColorConstant.primeryColor,
+                            ),
+                            border: UnderlineInputBorder(
+                                borderSide:
+                                BorderSide(color: ColorConstant.backgroundColor)),
+                            disabledBorder: UnderlineInputBorder(
+                                borderSide:
+                                BorderSide(color: ColorConstant.primeryColor)),
+                            enabledBorder: UnderlineInputBorder(
+                                borderSide:
+                                BorderSide(color: ColorConstant.primeryColor)),
+                            errorBorder: UnderlineInputBorder(
+                                borderSide:
+                                BorderSide(color: ColorConstant.primeryColor)),
+                          ),
+                        ),
+                        const SizedBox(
+                          height: 50,
+                        ),
+                        SizedBox(
+                          width: Get.width,
+                          child: ElevatedButton(
+                            style: ElevatedButton.styleFrom(
+                              backgroundColor: ColorConstant.backgroundColor
+                            ),
+                              onPressed: () {
+                                Get.toNamed(Routes.HOME);
+                              },
+                              child: const Text(
+                                "Sign In",
+                                style: TextStyle(color: ColorConstant.primeryColor),
+                              )),
+                        )
+                      ],
+                    )),
+              ),
+              SizedBox(
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      const Text("Don`t have an account ",style: TextStyle(color: ColorConstant.backgroundColor),),
+                      InkWell(
+                          onTap: () {},
+                          child: const Text(
+                            "SIGNUP",
+                            style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                color: ColorConstant.primeryColor),
+                          )),
+                    ],
                   )),
-            ],
-          )),
-        ]));
+            ])
+
+          ],
+        )
+
+
+            );
   }
 }
