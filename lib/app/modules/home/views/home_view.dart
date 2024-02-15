5e56bb5a98b37eebe4ffa20feedf1d5728404215
amanueldemelash12 <amanueldemelash12@gmail.com>
2024-02-15 17:25:03 +0300

    botton nav bar

--- lib/app/modules/home/views/home_view.dart
+++ lib/app/modules/home/views/home_view.dart
@@ -1,5 +1,6 @@
 
 import 'package:flutter/material.dart';
+import 'package:flutter_snake_navigationbar/flutter_snake_navigationbar.dart';
 import 'package:flutter_vendure_stor/app/constants/colorConstant.dart';
 import 'package:flutter_vendure_stor/app/modules/account/views/account_view.dart';
 import 'package:flutter_vendure_stor/app/modules/home/views/home_dashbord_view.dart';
@@ -12,41 +13,70 @@ class HomeView extends GetView<HomeController> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      bottomNavigationBar:Container(
-       //   margin:const EdgeInsets.only(bottom: 10,right: 10,left: 10),
-        //  decoration: BoxDecoration(
-        //       borderRadius: BorderRadius.circular(30),
-        //       color: Colors.transparent
-        //   ),
-          child: Obx(() =>
-              ClipRRect(
-                borderRadius:const BorderRadius.only(topLeft: Radius.circular(20),topRight: Radius.circular(20)),
-                child: NavigationBar(
-                    selectedIndex:controller.intialPage.value,
-                    elevation: 10,
-                    indicatorColor: ColorConstant.primeryColor,
-                    onDestinationSelected: (value) async {
-                      controller.intialPage.value=value;
-                      controller.pageController.jumpToPage(value);
-                      },
-                    backgroundColor: ColorConstant.backgroundColor,
-                    labelBehavior: NavigationDestinationLabelBehavior.onlyShowSelected,
-                    destinations:const[
-                      NavigationDestination(icon:Icon(Icons.home), label: "home"),
-                      NavigationDestination(icon:Icon(Icons.favorite), label: "favorite"),
-                      Badge(alignment: Alignment.centerRight,
-                          backgroundColor: ColorConstant.primeryColor,
-                          smallSize: 30,
-                          offset: Offset(-25,-12),
-                          label: Text("3"),
-                          child: NavigationDestination(icon:Icon(Icons.card_travel_rounded), label: "cart")),
-                      NavigationDestination(icon:Icon(Icons.person), label: "account"),
-                    ]),
-              ),)
-      ),
-      body:
-         PageView(
-          controller:controller.pageController,
+        bottomNavigationBar:Obx(() =>
+        SnakeNavigationBar.color(
+          behaviour:SnakeBarBehaviour.floating,
+          snakeShape:SnakeShape.rectangle,
+          //shape:ShapeBorder,
+          padding:const EdgeInsets.all(15) ,
+          ///configuration for SnakeNavigationBar.color
+          snakeViewColor:ColorConstant.primeryColor,
+          selectedItemColor:ColorConstant.backgroundColor,
+          ///configuration for SnakeNavigationBar.gradient
+          //snakeViewGradient: selectedGradient,
+          //selectedItemGradient: snakeShape == SnakeShape.indicator ? selectedGradient : null,
+          //unselectedItemGradient: unselectedGradient,
+          showUnselectedLabels:true,
+          showSelectedLabels: true,
+          currentIndex:controller.intialPage.value,
+          onTap:(value) {
+            controller.intialPage.value=value;
+            controller.pageController.jumpToPage(value);
+
+          },
+            items:const [
+            BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
+            BottomNavigationBarItem(icon: Icon(Icons.favorite), label: 'Favorite'),
+            BottomNavigationBarItem(icon: Icon(Icons.shopping_cart), label: 'Cart'),
+            BottomNavigationBarItem(icon: Icon(Icons.person), label: 'Account'),
+            ],
+            ),),
+
+            // Container(
+            //    margin:const EdgeInsets.only(bottom: 10,right: 10,left: 10),
+            //    decoration: BoxDecoration(
+            //         borderRadius: BorderRadius.circular(30),
+            //         color: Colors.transparent
+            //     ),
+            //     child: Obx(() =>
+            //         ClipRRect(
+            //           borderRadius:BorderRadius.circular(40),
+            //           child: NavigationBar(
+            //               selectedIndex:controller.intialPage.value,
+            //               elevation: 10,
+            //               indicatorColor: ColorConstant.primeryColor,
+            //               onDestinationSelected: (value) async {
+            //                 controller.intialPage.value=value;
+            //                 controller.pageController.jumpToPage(value);
+            //                 },
+            //               backgroundColor: ColorConstant.backgroundColor,
+            //               labelBehavior: NavigationDestinationLabelBehavior.onlyShowSelected,
+            //               destinations:const[
+            //                 NavigationDestination(icon:Icon(Icons.home), label: "home"),
+            //                 NavigationDestination(icon:Icon(Icons.favorite), label: "favorite"),
+            //                 Badge(alignment: Alignment.centerRight,
+            //                     backgroundColor: ColorConstant.primeryColor,
+            //                     smallSize: 30,
+            //                     offset: Offset(-25,-12),
+            //                     label: Text("3"),
+            //                     child: NavigationDestination(icon:Icon(Icons.card_travel_rounded), label: "cart")),
+            //                 NavigationDestination(icon:Icon(Icons.person), label: "account"),
+            //               ]),
+            //         ),)
+            // ),
+            body:
+            PageView(
+            controller:controller.pageController,
           physics:const NeverScrollableScrollPhysics(),
           children: [
             const HomeDashbordView(),
