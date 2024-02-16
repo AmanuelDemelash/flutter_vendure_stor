62ac821a984aa0940095942b4524a6c27fb0ede1
amanueldemelash12 <amanueldemelash12@gmail.com>
2024-02-16 11:52:39 +0300

    Sign up page

--- lib/app/modules/auth/views/sign_up_view.dart
+++ lib/app/modules/auth/views/sign_up_view.dart
@@ -1,9 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_vendure_stor/app/modules/auth/controllers/auth_controller.dart';
 import 'package:get/get.dart';
 import '../../../constants/colorConstant.dart';
+import '../../../routes/app_pages.dart';
 
-class SignUpView extends GetView {
-  const SignUpView({Key? key}) : super(key: key);
+class SignUpView extends GetView<AuthController>{
+ const  SignUpView({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -19,24 +22,182 @@ class SignUpView extends GetView {
           Container(
             width: Get.width,
             height: Get.height,
-            color: ColorConstant.primeryColor.withOpacity(0.7),
+            color: ColorConstant.primeryColor.withOpacity(0.6),
           ),
-          Container(
+          Positioned(
             width: Get.width,
             height: Get.height,
-            margin:const EdgeInsets.all(20),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              mainAxisAlignment: MainAxisAlignment.center,
+            child:
+            ListView(
               children: [
-                IconButton(onPressed:() => Get.back(), icon:const Icon(Icons.arrow_back_ios,color: ColorConstant.iconColor,)),
+                        const Text(
+                          "Bazaar",
+                          textAlign: TextAlign.center,
+                          style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              color: ColorConstant.backgroundColor,
+                              fontSize:36,
+                              letterSpacing:7),
+                        ),
+                        const SizedBox(
+                          height: 40,
+                        ),
+                         Container(
+                            width: Get.width,
+                            padding: const EdgeInsets.all(30),
+                            child: Form(
+                                child: Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    const Text("Sign Up",style:TextStyle(color: ColorConstant.backgroundColor,fontWeight: FontWeight.w400,fontSize: 20),),
+                                    const SizedBox(
+                                        height:30
+                                    ),
+                                    TextFormField(
+                                      cursorColor: ColorConstant.backgroundColor,
+                                      style:const TextStyle(color: ColorConstant.backgroundColor),
+                                      decoration:InputDecoration(
+                                        filled: false,
+                                        hintText: "full name",
+                                        hintStyle: TextStyle(color: ColorConstant.backgroundColor.withOpacity(0.9)),
+                                        prefixIcon:const Icon(
+                                          Icons.person,
+                                          color: ColorConstant.backgroundColor,
+                                        ),
+                                        border:textUnderlineInputBorder(),
+                                        focusedBorder:textUnderlineInputBorder(),
+                                        disabledBorder:textUnderlineInputBorder(),
+                                        enabledBorder:textUnderlineInputBorder(),
+                                        errorBorder:const UnderlineInputBorder(
+                                            borderSide:
+                                            BorderSide(color: Colors.red)),
+                                      ),
+                                    ),
+                                    const SizedBox(
+                                      height: 15,
+                                    ),
+                                    TextFormField(
+                                      cursorColor: ColorConstant.backgroundColor,
+                                      style:const TextStyle(color: ColorConstant.backgroundColor),
+                                      decoration:InputDecoration(
+                                        filled: false,
+                                        hintText: "email",
+                                        hintStyle: TextStyle(color: ColorConstant.backgroundColor.withOpacity(0.9)),
+                                        prefixIcon:const Icon(
+                                          Icons.email,
+                                          color: ColorConstant.backgroundColor,
+                                        ),
+                                        border:textUnderlineInputBorder(),
+                                        focusedBorder:textUnderlineInputBorder(),
+                                        disabledBorder:textUnderlineInputBorder(),
+                                        enabledBorder:textUnderlineInputBorder(),
+                                        errorBorder:const UnderlineInputBorder(
+                                            borderSide:
+                                            BorderSide(color: Colors.red)),
+                                      ),
+                                    ),
+                                    const SizedBox(
+                                      height: 15,
+                                    ),
+                                    TextFormField(
+                                      cursorColor: ColorConstant.backgroundColor,
+                                      style:const TextStyle(color: ColorConstant.backgroundColor),
+                                      decoration:InputDecoration(
+                                        filled: false,
+                                        hintText: "phone",
+                                        hintStyle: TextStyle(color: ColorConstant.backgroundColor.withOpacity(0.9)),
+                                        prefixIcon:const Icon(
+                                          Icons.phone,
+                                          color: ColorConstant.backgroundColor,
+                                        ),
+                                        border:textUnderlineInputBorder(),
+                                        focusedBorder:textUnderlineInputBorder(),
+                                        disabledBorder:textUnderlineInputBorder(),
+                                        enabledBorder:textUnderlineInputBorder(),
+                                        errorBorder:const UnderlineInputBorder(
+                                            borderSide:
+                                            BorderSide(color: Colors.red)),
+                                      ),
+                                    ),
+                                    const SizedBox(
+                                      height: 15,
+                                    ),
+                                    Obx(() =>
+                                        TextFormField(
+                                          obscureText:controller.showPassword.value,
+                                          cursorColor: ColorConstant.backgroundColor,
+                                          style:const TextStyle(color: ColorConstant.backgroundColor),
+                                          decoration: InputDecoration(
+                                            filled: false,
+                                            hintText: "password",
+                                            hintStyle: TextStyle(color: ColorConstant.backgroundColor.withOpacity(0.9)),
+                                            prefixIcon:const Icon(
+                                              Icons.lock,
+                                              color: ColorConstant.backgroundColor,
+                                            ),
+                                            suffixIcon:controller.showPassword.value?
+                                            IconButton(onPressed:() => controller.showPassword.value=false, icon:const Icon(Icons.visibility_off,color: ColorConstant.backgroundColor,)):
+                                            IconButton(onPressed:() => controller.showPassword.value=true, icon:const Icon(Icons.visibility,color: ColorConstant.backgroundColor,)),
+                                            border:textUnderlineInputBorder(),
+                                            focusedBorder: textUnderlineInputBorder(),
+                                            disabledBorder:textUnderlineInputBorder(),
+                                            enabledBorder:textUnderlineInputBorder(),
+                                            errorBorder:const UnderlineInputBorder(
+                                                borderSide:
+                                                BorderSide(color: Colors.red)),
+                                          ),
+                                        ),),
+                                    const SizedBox(
+                                      height: 50,
+                                    ),
+                                    SizedBox(
+                                      width: Get.width,
+                                      child: ElevatedButton(
+                                          style: ElevatedButton.styleFrom(
+                                              backgroundColor: ColorConstant.backgroundColor,
+                                              padding:const EdgeInsets.all(15),
+                                              elevation: 0
+                                          ),
+                                          onPressed: () {
+                                            Get.toNamed(Routes.HOME);
+                                          },
+                                          child: const Text(
+                                            "Sign Up",
+                                            style: TextStyle(color: ColorConstant.primeryColor),
+                                          )),
+                                    )
+                                  ],
+                                )),
 
-              ],
+                        ),
+                        Container(
+                            margin:const EdgeInsets.only(bottom: 40),
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                Text("Already have an account ",style: TextStyle(color: ColorConstant.backgroundColor.withOpacity(0.8)),),
+                                InkWell(
+                                    onTap:() => Get.back(),
+                                    child: const Text(
+                                      "SIGN IN",
+                                      style: TextStyle(
+                                          fontWeight: FontWeight.bold,
+                                          color: ColorConstant.backgroundColor),
+                                    )),
+                              ],
+                            )),
+                      ]
             ),
 
           )
         ],
       )
     );
+  }
+
+  UnderlineInputBorder textUnderlineInputBorder() {
+    return const UnderlineInputBorder(
+                                          borderSide:
+                                          BorderSide(color: ColorConstant.backgroundColor));
   }
 }
