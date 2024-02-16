b2040de0e40341efe4432ca6929978ac31ba9dde
amanueldemelash12 <amanueldemelash12@gmail.com>
2024-02-16 12:14:21 +0300

    cart

--- lib/app/modules/cart/views/cart_view.dart
+++ lib/app/modules/cart/views/cart_view.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_vendure_stor/app/constants/colorConstant.dart';
 
 import 'package:get/get.dart';
 
@@ -13,13 +14,53 @@ class CartView extends GetView<CartController> {
         automaticallyImplyLeading: false,
         title: const Text('Cart',style: TextStyle(fontSize: 27),),
         centerTitle: false,
+        elevation: 0,
+        excludeHeaderSemantics:false,
       ),
-      body: const Center(
-        child: Text(
-          'CartView is working',
-          style: TextStyle(fontSize: 20),
-        ),
-      ),
+      body: Column(
+        children: [
+          Expanded(
+            child: ListView.builder(
+              itemCount: 3,
+              itemBuilder:(context, index) =>
+                  SizedBox(
+                    width: Get.width,
+                    child: Card(
+                      margin:const EdgeInsets.all(10),
+                      color: ColorConstant.iconColor,
+                      elevation: 10,
+                      shadowColor: ColorConstant.primeryColor.withOpacity(0.1),
+                    ),
+                  ) ,
+            ),
+          ),
+          Container(
+            width: Get.width,
+            height: 200,
+            padding:const EdgeInsets.all(15),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                Row(children: [
+                  Expanded(
+                    child: TextField(
+                      decoration: InputDecoration(
+                        filled: true,
+                        fillColor: ColorConstant.primeryColor.withOpacity(0.2),
+                        hintText: "promo code hear",
+                        border: InputBorder.none
+                      ),
+                    ),
+                  ),
+                  Text("Apply")
+                ],)
+
+              ],
+            ),
+
+          )
+        ],
+      )
     );
   }
 }
